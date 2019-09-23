b2124ad502854edd59a7fe95e228bad4d8647742
Satria Aji Putra <satmaxt@outlook.com>
2019-09-23 12:38:10 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan1/biodata/PBO210118068Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Pada latihan 1 ini, yaitu membuat program untuk
+ * menampilkan biodata diri
+ */
+public class PBO210118068Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: SATRIA AJI PUTRA");
+        System.out.println("ALAMAT\t\t: KP Selakopi, RT 002 RW 011");
+        System.out.println("TEMPAT LAHIR\t: Sukabumi");
+        System.out.println("TANGGAL LAHIR\t: 22 Juli 1999");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Diskusi Agama & Coding");
+        System.out.println("MAKES\t\t: Angeun");
+        System.out.println("MIKES\t\t: Gula asem");
+    }
+    
+}
